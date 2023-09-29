f65f6832bc7b6e3a726751be8ba62e31696adcae
Kai <kaiger.lin@gmail.com>
2023-09-29 12:34:05 -0400

    HW 4 Upload

---
+++ solution-hw4/js/rollsData.js
@@ -0,0 +1,32 @@
+const rolls = {
+    "Original": {
+        "name": "Original Cinnamon Roll",
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "name": "Apple Cinnamon Roll",
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "name": "Raisin Cinnamon Roll",
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "name": "Walnut Cinnamon Roll",
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "name": "Double Chocolate Cinnamon Roll",
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "name": "Strawberry Cinnamon Roll",
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+  };
